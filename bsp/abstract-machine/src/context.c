cbc0166d49aea26615e48a50238e1d73e6c94c75
wannan123 <1397886153@qq.com>
2024-04-24 10:41:25 -0400

    add rt_hw_stack_init

--- bsp/abstract-machine/src/context.c
+++ bsp/abstract-machine/src/context.c
@@ -24,8 +24,32 @@ void rt_hw_context_switch(rt_ubase_t from, rt_ubase_t to) {
 void rt_hw_context_switch_interrupt(void *context, rt_ubase_t from, rt_ubase_t to, struct rt_thread *to_thread) {
   assert(0);
 }
+typedef struct {
+    void* tentry_;
+    void* parameter_;
+    void* texit_;
+} CombinedArgs;
+
+static void entry(void *args){
+  CombinedArgs* arg = (CombinedArgs*)args;
+  void* parameter = arg->parameter_;
+  void (*tentry)(void *) = (void (*)(void *))arg->tentry_;
+  void (*texit)(void) = arg->texit_;
+  tentry(parameter);
+  texit();
+}
 
 rt_uint8_t *rt_hw_stack_init(void *tentry, void *parameter, rt_uint8_t *stack_addr, void *texit) {
-  assert(0);
-  return NULL;
+  uintptr_t addr = (uintptr_t)stack_addr;
+  uintptr_t align = sizeof(uintptr_t);
+  uintptr_t aligned_addr = (addr + align - 1) & ~(align - 1);
+  stack_addr = (rt_uint8_t *)aligned_addr;
+
+  CombinedArgs* args = (CombinedArgs*)malloc(sizeof(CombinedArgs));
+  args->tentry_ = tentry;
+  args->parameter_ = parameter;
+  args->texit_ = texit;
+  Context *kcontextt = kcontext((Area) { stack_addr-2048, stack_addr }, entry, (void *)args);
+  //assert(0);
+  return (rt_uint8_t *)kcontextt;
 }
--- bsp/abstract-machine/src/libc.c
+++ bsp/abstract-machine/src/libc.c
@@ -1,6 +1,5 @@
 #include <klib.h>
 #include <rtthread.h>
-#include <ctype.h>
 char *strchr(const char *s, int c) {
     while (*s != (char)c) {
         if (!*s++) {
@@ -24,18 +23,15 @@ char *strrchr(const char *s, int c) {
 char *strstr(const char *haystack, const char *needle) {
   return rt_strstr(haystack, needle);
 }
-/*
+int is_space(char c) {
+    return c == ' ';
+}
 long strtol(const char *restrict nptr, char **restrict endptr, int base) {
-  assert(0);
-}*/
-
-
-long strtol(const char *nptr, char **endptr, int base) {
     int neg = 0;
     long val = 0;
 
     // Skip whitespace
-    while (isspace(*nptr)) nptr++;
+    while (is_space(*nptr)) nptr++;
 
     // Check for optional sign
     if (*nptr == '+' || *nptr == '-') {
@@ -73,6 +69,51 @@ long strtol(const char *nptr, char **endptr, int base) {
     // Apply sign and return result
     return (neg ? -val : val);
 }
+
+
+// long strtol(const char *restrict nptr, char **restrict endptr, int base){
+//     int neg = 0;
+//     long val = 0;
+
+//     // Skip whitespace
+//     while (isspace(*nptr)) nptr++;
+
+//     // Check for optional sign
+//     if (*nptr == '+' || *nptr == '-') {
+//         neg = (*nptr == '-');
+//         nptr++;
+//     }
+
+//     // Check for optional "0x" prefix if base is 16
+//     if ((base == 0 || base == 16) && (nptr[0] == '0' && (nptr[1] == 'x' || nptr[1] == 'X'))) {
+//         nptr += 2;
+//         base = 16;
+//     }
+
+//     // If no base provided, assume 10
+//     if (base == 0) base = 10;
+
+//     // Convert characters to value
+//     while (1) {
+//         int digit;
+
+//         if (*nptr >= '0' && *nptr <= '9') digit = *nptr - '0';
+//         else if (*nptr >= 'a' && *nptr <= 'z') digit = *nptr - 'a' + 10;
+//         else if (*nptr >= 'A' && *nptr <= 'Z') digit = *nptr - 'A' + 10;
+//         else break;
+
+//         if (digit >= base) break;
+
+//         val = val * base + digit;
+//         nptr++;
+//     }
+
+//     // Store end pointer if requested
+//     if (endptr) *endptr = (char *)nptr;
+
+//     // Apply sign and return result
+//     return (neg ? -val : val);
+// }
 
 char *strncat(char *dst, const char *src, size_t sz) {
     char *ptr = dst;
